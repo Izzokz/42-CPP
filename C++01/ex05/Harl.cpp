f637f909e3ae5640cfee0f728239c29badd495ef
Kylian Zhen-Claverie <kzhen-cl@2E2.42angouleme.fr>
2025-05-08 19:27:34 +0200

    Fixed C++01/ex05

--- C++01/ex05/Harl.cpp
+++ C++01/ex05/Harl.cpp
@@ -12,12 +12,12 @@
 
 #include "Harl.hpp"
 
-void	Harl::debug(void)
+void	Harl::info(void)
 {
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\n";
 }
 
-void	Harl::info(void)
+void	Harl::debug(void)
 {
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
 }
