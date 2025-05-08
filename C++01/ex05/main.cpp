8a3c851ba805b9b19ac0c01468246e4d0579e7ff
Kylian Zhen-Claverie <kzhen-cl@2E2.42angouleme.fr>
2025-05-08 17:43:27 +0200

    Better main (C++01/ex05)

--- C++01/ex05/main.cpp
+++ C++01/ex05/main.cpp
@@ -12,13 +12,17 @@
 
 #include "Harl.hpp"
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
 	Harl	a = Harl();
 
+	(void) argc;
 	a.complain("INFO");
 	a.complain("DEBUG");
 	a.complain("WARNING");
 	a.complain("ERROR");
 	a.complain("NANANANA");
+	std::cout << '\n';
+	while (*++argv)
+		a.complain((std::string)*argv);
 }
