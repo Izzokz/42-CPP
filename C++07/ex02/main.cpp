177bb9c839f48a8b3d8482198cee3edc92a5a404
Izzokz <askilola@hotmail.fr>
2025-11-02 15:48:48 +0100

    C++07/ex02 Better main

--- C++07/ex02/main.cpp
+++ C++07/ex02/main.cpp
@@ -77,17 +77,5 @@ int	main(void)
 	wahou[0].remove(1);
 	wahou.print();
 
-/*
-	try
-	{
-		Array<int>	a(-1);
-		a.pushFront(1);
-	}
-	catch (const std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-*/
-
 	std::cout << "\naEND" << std::endl;
 }
